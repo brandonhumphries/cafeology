da246879858e39546a3553817e5cc736536d73d3
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-08-03 13:50:52 -0400

    can capture the survey results in an object

---
+++ main.js
@@ -0,0 +1,24 @@
+var closeSurveryButton = document.querySelector('[class="lightbox-close-button"]');
+closeSurveryButton.addEventListener('click', function(){
+    var surveyLightbox = document.querySelector('[class="lightbox-questionnaire-container"]')
+    surveyLightbox.classList.add('hidden');
+});
+
+
+var importSurveyInformation = document.querySelector('[data-coffee-questionnaire="form"]');
+importSurveyInformation.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var temperatureInput = document.querySelector('[name="temperature"]:checked');
+    var strengthInput = document.querySelector('[name="strength-level-input"]');
+    var creamInput = document.querySelector('[name="cream-level-input"]');
+    var sweetnessInput = document.querySelector('[name="sweetness-level-input"]');
+    var flavorInput = document.querySelector('[name="flavor-input"]:checked');
+
+    var surveyResults = {temperature: '', strength: '', cream: '', sweetness: '', flavor: ''};
+    surveyResults.temperature = temperatureInput.value;
+    surveyResults.strength = strengthInput.value;
+    surveyResults.cream = creamInput.value;
+    surveyResults.sweetness = sweetnessInput.value;
+    surveyResults.flavor = flavorInput.value;
+    console.log(surveyResults);
+});
