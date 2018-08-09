d6b5ee30af5d962e7457a5dd0609bfbacfe26a43
Mmsimpson <Marshall1044@gmail.com>
2018-08-09 08:21:56 -0400

    edit styling to the evevents page

--- main.js
+++ main.js
@@ -108,12 +108,13 @@ var eventsRow = function(events) {
     var dateBox = document.createElement('div')
     var titleBox = document.createElement('div')
 
+    url.classList.add('anchorTag')
     url.setAttribute('href', events.url)
     url.target = ('_blank')
-    titleBox.textContent = 'name: ' + events.name.text.substr(0,80)
-    descripBox.textContent = ' description: ' + events.description.text.substr(0,80)
-    dateBox.textContent = ' date: ' + events.start.local
-    
+    titleBox.textContent =  events.name.text.substr(0,120)
+    descripBox.textContent = events.description.text.substr(0,120)
+    dateBox.textContent = events.start.local
+
     var description = events.description.text
     descripBox.addEventListener('click', function() {
         descripBox.textContent = description
